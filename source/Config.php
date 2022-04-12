473c5ba9adfd7307d2766a7578248e2bd1691c34
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-04-12 14:54:17 +0200

    update redirectUri Google Api

--- source/Config.php
+++ source/Config.php
@@ -3,5 +3,5 @@
 define("GOOGLE", [
   'clientId'     => '657481124847-ud4upjlevq7ftocr1c7a96mjmfp9uict.apps.googleusercontent.com',
   'clientSecret' => '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',
-  'redirectUri'  => 'http://localhost/projects/php-tips/'
+  'redirectUri'  => 'http://localhost/projects/php-tips/google-auth/'
 ]);
